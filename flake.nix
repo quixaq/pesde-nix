3b4a8a22824dc4a4c4448b402b69a0f95dff62ab
Quixaq <quixaq@tutamail.com>
2026-04-20 16:40:11 +0200

    refactor: use flake-utils for system

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     crane.url = "github:ipetkov/crane";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
@@ -11,13 +12,45 @@
       self,
       nixpkgs,
       crane,
+      flake-utils,
       ...
     }:
-    let
-      supportedSystems = nixpkgs.lib.systems.flakeExposed;
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    in
-    {
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.mkLib pkgs;
+
+        src = pkgs.fetchFromGitHub {
+          owner = "pesde-pkg";
+          repo = "pesde";
+          rev = "e57b4c2db9eaf295c8af998212f427ea039ed46e";
+          hash = "sha256-+8SneWw3UQwXg1IV1zn0OM1ySAJpcvMqyoQd7eYAarE=";
+        };
+      in
+      {
+        packages.default = craneLib.buildPackage {
+          inherit src;
+
+          cargoExtraArgs = "--features bin";
+
+          nativeBuildInputs = [
+            pkgs.pkg-config
+          ];
+
+          buildInputs = [
+            pkgs.openssl
+            pkgs.dbus
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.libiconv
+            pkgs.darwin.apple_sdk.frameworks.Security
+            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+          ];
+        };
+      }
+    )
+    // {
       nixosModules.default =
         { pkgs, ... }:
         {
@@ -25,41 +58,5 @@
             self.packages.${pkgs.system}.default
           ];
         };
-
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          craneLib = crane.mkLib pkgs;
-
-          src = pkgs.fetchFromGitHub {
-            owner = "pesde-pkg";
-            repo = "pesde";
-            rev = "e57b4c2db9eaf295c8af998212f427ea039ed46e";
-            hash = "sha256-+8SneWw3UQwXg1IV1zn0OM1ySAJpcvMqyoQd7eYAarE=";
-          };
-        in
-        {
-          default = craneLib.buildPackage {
-            inherit src;
-
-            cargoExtraArgs = "--features bin";
-
-            nativeBuildInputs = [
-              pkgs.pkg-config
-            ];
-
-            buildInputs = [
-              pkgs.openssl
-              pkgs.dbus
-            ]
-            ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-              pkgs.libiconv
-              pkgs.darwin.apple_sdk.frameworks.Security
-              pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-            ];
-          };
-        }
-      );
     };
 }
